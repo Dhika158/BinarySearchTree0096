a58276960e82bbb4199572c17e05dc62e6767987
Dhika158 <andhikanurjamil7@gmail.com>
2025-06-26 18:39:55 +0700

    Make the rigth child of parent point to the new node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -68,7 +68,7 @@ class BinaryTree
         // step 7: If the value in the data field of the new node is greater than that of the parent
         else if (x > parent->info)
         {
-        
+        // 7a: Make the rigth child of parent point to the new node 
         }
     }
 }
